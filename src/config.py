21050596d5426b057d5c6e6525a874d320ed2fba
tolios <apostolos.kakampakos@gmail.com>
2023-03-24 23:46:53 +0200

    added argparse in requirements.txt

--- src/config.py
+++ src/config.py
@@ -1,3 +1,3 @@
-MINING_DIFFICULTY = 4 #num of zeros infront of block hash | 4, 5 
-CAPACITY = 10 #num of valid transactions to mine a block | 1, 5, 10
+MINING_DIFFICULTY = 5 #num of zeros infront of block hash | 4, 5 
+CAPACITY = 5 #num of valid transactions to mine a block | 1, 5, 10
 N = 5 #num of clients | 5, 10
